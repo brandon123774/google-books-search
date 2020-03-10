422dd273de765f85d9d94fd9ef3ff83c9a47b291
Brandon Shankar <bshankar12@comcast.net>
2020-03-10 12:19:33 -0700

    edited file paths

---
+++ client/src/components/SaveBtn/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+// need to be able to save books
+function SaveBtn({ type = "default", className, children, onClick }) {
+
+    // saves book to db and shows modal
+    const click = () => {
+        onClick()
+    };
+
+    return (
+            <button onClick={click} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
+                {children}
+            </button>
+
+    );
+};
+
+export default SaveBtn;
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import API from "../utils/APi";
+import { Container, Row, Col } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+
+class Save extends Component {
+
+    // saved books
+    state = {
+        savedBooks: []
+    };
+
+    // loads saved books when Saved page loads
+    componentDidMount() {
+        this.loadBooks();
+    };
+
+    // loads books from db
+    loadBooks = event => {
+
+        API.getBooks()
+            .then(res => {
+                this.setState({ savedBooks: res.data }, function () {
+                    console.log(this.state.savedBooks);
+                })
+            })
+            .catch(err => console.log(err))
+    };
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col size="xs-12">
+                            <List>
+                                {this.state.savedBooks.map(book => {
+                                    return (
+                                        <ListItem
+
+                                            key={book.id}
+                                            id={book.id}
+                                            title={book.volumeInfo.title}
+                                            authors={book.volumeInfo.authors}
+                                            link={book.volumeInfo.infoLink}
+                                            description={book.volumeInfo.description}
+                                            loadBooks={this.loadBooks}
+                                        />
+                                    );
+                                })}
+                            </List>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    };
+
+};
+
+export default Save;
